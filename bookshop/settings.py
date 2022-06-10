f4e1abe260f2f4ae4cb83b34a5bda37aa3a973c5
bernardo <bernarrr69@gmail.com>
2022-06-10 20:59:00 +0200

    heroku cambiado

--- bookshop/settings.py
+++ bookshop/settings.py
@@ -21,7 +21,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['obscure-river-38918.herokuapp.com', '127.0.0.1', 'Localhost']
+ALLOWED_HOSTS = ['psiextraordinaria.herokuapp.com', '127.0.0.1', 'Localhost']
 
 EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
 EMAIL_FILE_PATH = '/tmp/app−messages'  # change this to a proper location
